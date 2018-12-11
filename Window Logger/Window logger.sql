fe27f8b3a86bfd5f04ba41b091199fb95199d7d5
Paul Watson <psjw12@gmail.com>
2018-12-11 17:38:31 +0000

    Added "Window logger.sql"

---
+++ Window Logger/Window logger.sql
@@ -0,0 +1,3 @@
+CREATE TABLE ActiveWindowProcessLog (KeyId INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL, Date, TEXT (10) NOT NULL, Time TEXT (8) NOT NULL, Process STRING NOT NULL, WindowTitle VARCHAR (200));
+
+CREATE INDEX ActiveWindowProcessLog_Date ON ActiveWindowProcessLog (Date);
